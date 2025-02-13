745be55d3a950a43f7603277a3d180f1f5db8fa7
CorentinSaccoman <155541325+CorentinSaccoman@users.noreply.github.com>
2025-02-13 11:27:41 +0100

    Question 3 : deuxième méthode

--- src/main/java/exo1/EnvoiPhoto.java
+++ src/main/java/exo1/EnvoiPhoto.java
@@ -1,7 +1,10 @@
 package exo1;
 
-public class EnvoiPhoto extends ServiceEnvoiPhoto implements Envoi{
+public class EnvoiPhoto implements Envoi{
+    private final ServiceEnvoiPhoto serviceEnvoi = new ServiceEnvoiPhoto();
+
     public void envoi(Contact c, String image){
-        envoiPhoto(c, image);
+        // envoiPhoto(c, image);            // Méthode 1 (si EnvoiPhoto étend ServiceEnvoiPhoto)
+        serviceEnvoi.envoiPhoto(c, image);  // Méthode 2 (sans étendre ServiceEnvoiPhoto)
     }
 }
