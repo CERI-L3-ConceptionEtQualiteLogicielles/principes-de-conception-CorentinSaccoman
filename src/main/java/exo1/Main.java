2fe3c86b5ab12fbcc7982212beb5a81c55c927ab
CorentinSaccoman <155541325+CorentinSaccoman@users.noreply.github.com>
2025-02-13 11:05:01 +0100

    Création d'un petit main

---
+++ src/main/java/exo1/Main.java
@@ -0,0 +1,22 @@
+package exo1;
+
+public class Main {
+    public static void main(String [] args) {
+        Contact papa = new Contact("Papa", "0123456789");
+        Contact maman = new Contact("Maman", "9876543210");
+
+        Annuaire contacts = new Annuaire();
+        contacts.ajouteContact(papa);
+        contacts.ajouteContact(maman);
+
+        contacts.afficheContacts();
+
+        EnvoiSms sms = new EnvoiSms();
+
+        sms.envoi(papa, "Coucou");
+
+        EnvoiMail mail = new EnvoiMail();
+        mail.envoi(maman, "Coucou (version mail)");
+    }
+
+}
